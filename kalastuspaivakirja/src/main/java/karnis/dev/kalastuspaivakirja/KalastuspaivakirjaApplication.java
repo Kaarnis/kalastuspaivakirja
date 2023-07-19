2976f71dbf4256b7484831b3e774b6463c0b7e03
kaarnis <karnarantaveeti@gmail.com>
2023-07-19 21:34:05 +0300

    first commit

---
+++ kalastuspaivakirja/src/main/java/karnis/dev/kalastuspaivakirja/KalastuspaivakirjaApplication.java
@@ -0,0 +1,13 @@
+package karnis.dev.kalastuspaivakirja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KalastuspaivakirjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KalastuspaivakirjaApplication.class, args);
+	}
+
+}
---
+++ kalastuspaivakirja/src/test/java/karnis/dev/kalastuspaivakirja/KalastuspaivakirjaApplicationTests.java
@@ -0,0 +1,13 @@
+package karnis.dev.kalastuspaivakirja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KalastuspaivakirjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
